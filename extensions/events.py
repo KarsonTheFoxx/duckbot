980e7876c418bcbf4fc957a5d0d7bda27454ec30
KarsonTheFoxx <95767655+KarsonTheFoxx@users.noreply.github.com>
2024-12-13 09:37:09 -0700

    Update events.py
    Manually doing this because github keeps deleting my access tokens and I'm getting tired of having to regenerate them of every commit

--- extensions/events.py
+++ extensions/events.py
@@ -8,17 +8,23 @@ plugin = Plugin()
 async def on_reaction_add(reaction:Reaction, _:User):
     with open("data.json", "r") as data_file:
         data = load(data_file)
-    
+
     if not data["starboard-channel-id"]:
         return
-    
+
     if reaction.count == data["starboard-threshold"] and reaction.emoji == "⭐":
-        reaction_channel = reaction.message.guild.get_channel(data["starboard-channel-id"])
-        
-        await reaction_channel.send(embed=Embed(title=f":star: Starboard #{data['starboard-count']+1}", description=reaction.message.content, color=Color.random()))
+        starboard_channel = reaction.message.guild.get_channel(data["starboard-channel-id"])
+
+        await starboard_channel.send(
+            content=reaction.message.author.mention,
+            embed = Embed(
+                title=f":star: Starboard #{data["starboard-count"]+1}", 
+                description=reaction.message.content, color=Color.random()).add_field(
+                    name="Jump to message", 
+                    value=reaction.message.jump_url).set_thumbnail(
+                        url=reaction.message.author.avatar.url))
 
         data["starboard-count"] += 1
-        
         with open("data.json", "w") as data_file:
             dump(data, data_file)
 
@@ -32,4 +38,4 @@ async def on_message(message:Message):
     else:
         await message.reply("Quack")
 
-setup, teardown = plugin.create_extension_handlers()
+setup, teardown = plugin.create_extension_handlers()    
